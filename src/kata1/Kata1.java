ab6b52ec833d68448cf1db72b1826af1317702e3
User <User@DESKTOP-PUEKB8G>
2021-09-24 14:27:45 +0100

    Actualización Kata 1: Limpio de import innecesarios

--- src/kata1/Kata1.java
+++ src/kata1/Kata1.java
@@ -1,13 +1,11 @@
 package kata1;
 
 import java.time.LocalDate;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
 
 public class Kata1 {
     public static void main(String[] args) {
         LocalDate birthdate = LocalDate.of(1999, 10, 10);
-        Person person1 = new Person("Antonio", birthdate);
+        Person person1 = new Person("Cecilia", birthdate);
         
         System.out.println(person1.getName() + " tiene " + person1.getAge() + " años ");
     }
