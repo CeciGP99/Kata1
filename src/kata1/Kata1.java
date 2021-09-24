17f57cfb757a94ab454ea9b0dcb9df6866b657c6
User <User@DESKTOP-PUEKB8G>
2021-09-24 14:25:41 +0100

    Actualización Kata 1: Cambiando Calendar a LocalDate

--- src/kata1/Kata1.java
+++ src/kata1/Kata1.java
@@ -6,7 +6,7 @@ import java.util.GregorianCalendar;
 
 public class Kata1 {
     public static void main(String[] args) {
-        LocalDate birthdate = LocalDate.of(1980, 1, 1);
+        LocalDate birthdate = LocalDate.of(1999, 10, 10);
         Person person1 = new Person("Antonio", birthdate);
         
         System.out.println(person1.getName() + " tiene " + person1.getAge() + " años ");
